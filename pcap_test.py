36d29125056eb6b3bff8210c035f4e2c3dede11d
tr3vv0p <trevop94@gmail.com>
2018-09-11 19:11:02 +0300

    ya

--- pcap_test.py
+++ pcap_test.py
@@ -8,9 +8,8 @@ str = ''
 for pkt in pcap:
     if Raw in pkt:
         str += bytes(pkt[Raw].__str__(), 'utf-8').decode('utf-8')[2]
-            #pkt[Raw].decode("utf-8")
 
 print(str)
-print(base64.b64decode('TnZGFjo+k4/vaiYaQcTD8sr8vPSh\THAWEzhslIntbnBOIpaq\FNG8Hf4vzfxRlsrB/Ek/wpWX0xDdECEiKrAKqER/LTAJ3sJ7aco='))
+b_dec = base64.b64decode(str[str.index('>:')+2:str.index(':<')])
+print(bytes_hex(b_dec).decode('utf-8'))
 
-str = '546e5a47466a6f2b6b342f76616959615163544438737238765053685c54484157457a68736c496e74626e424f497061715c464e4738486634767a6678526c7372422f456b2f7770575830784464454345694b72414b7145522f4c54414a33734a3761636f3d'
